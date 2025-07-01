dd0c4078d3ee1ee5bc929b3864db11b61cc2d937
fibgit <fibergitt@gmail.com>
2025-07-01 21:07:58 +0100

    updated config file and base class

---
+++ src/test/java/config/VideoGameConfig.java
@@ -0,0 +1,25 @@
+package config;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import org.junit.BeforeClass;
+
+public class VideoGameConfig {
+
+    @BeforeClass
+    public static void setup(){
+
+        RestAssured.requestSpecification = new RequestSpecBuilder()
+                .setBaseUri("https://videogamedb.uk/")
+                .setBasePath("api/v2")
+                .setContentType("application/json")
+                .addHeader("Accept", "application/json")
+                .build();
+
+        RestAssured.responseSpecification = new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .build();
+
+    }
+}
