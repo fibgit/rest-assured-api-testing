ca8da95cf263a12e68a027b11742fb50685e3954
fibgit <fibergitt@gmail.com>
2025-07-06 00:52:45 +0100

    GET calls for the Footbal API

--- src/test/java/FootballTests.java
+++ src/test/java/FootballTests.java
@@ -1,4 +1,37 @@
 import config.FootballConfig;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
+
 
 public class FootballTests extends FootballConfig {
+
+    private static final Log log = LogFactory.getLog(FootballTests.class);
+
+    @Test
+    public void getDetailsOfOneArea(){
+        given()
+                .queryParam("areas", 2076)
+        .when()
+                .get("/areas")
+                .then()
+                .log().body();
+
+    }
+
+    @Test
+    public void getDetailsOfMultipleAreas(){
+        String areaIds = "2076,2077,2080";
+
+        given()
+                .urlEncodingEnabled(false)
+                .queryParam("areas", areaIds)
+        .when()
+                .get("/areas")
+        .then()
+                .log().body();
+
+    }
 }
